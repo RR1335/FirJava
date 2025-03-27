5e7cc4303128c4589c016314e78e6759eebd199a
RR1335 <odyli@126.com>
2025-03-27 09:29:34 -0400

    run method and main

---
+++ Four/src/biz/baijing/methods/methoddef.java
@@ -0,0 +1,14 @@
+package biz.baijing.methods;
+
+public class methoddef {
+    public static void main(String[] args) {
+        runMethod();
+    }
+
+    // 定义一个方法
+    public static void runMethod() {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("!!!");
+    }
+}
