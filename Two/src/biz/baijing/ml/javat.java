51960d703e3455f42fda35b9b00c36755995f338
RR1335 <odyli@126.com>
2025-03-26 21:21:45 -0400

    Update javat.java

--- Two/src/biz/baijing/ml/javat.java
+++ Two/src/biz/baijing/ml/javat.java
@@ -11,5 +11,16 @@ public class javat {
 
         System.out.println(e);
         System.out.println(g);
+
+        int a = 100;
+        int b = 500;
+
+        byte a1 = (byte) a;
+        byte b1 = (byte) b;
+
+        System.out.println(a1);
+        // -12 , 强制转换类型可能导致计算错误
+        System.out.println(b1);
+
     }
 }
