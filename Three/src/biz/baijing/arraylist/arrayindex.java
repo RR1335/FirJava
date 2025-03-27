4c80204d15f2493a4809a41e6e8eae96322a4176
RR1335 <odyli@126.com>
2025-03-27 09:02:15 -0400

    array index bound

--- Three/src/biz/baijing/arraylist/arrayindex.java
+++ Three/src/biz/baijing/arraylist/arrayindex.java
@@ -10,7 +10,14 @@ public class arrayindex {
         System.out.println(stu[0]);
         System.out.println(stu[1]);
         // 没有定义的 初始化值 null  |  整数类型 0 ，浮点类型 0.0  ｜ 字符类型 /u0000 (空格）
-        // boolean  默认 false  |  引用数据类型 null 
+        // boolean  默认 false  |  引用数据类型 null
         System.out.println(stu[2]);
+
+        int[] arr = {1,2,3,4,5};
+        // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
+        // 索引超过长度为 5
+        System.out.println(arr[5]);
+
+
     }
 }
