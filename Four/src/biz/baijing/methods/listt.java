ffb97a54a1eb626a4f8f1dc38c1a57a51c89a4b1
RR1335 <odyli@126.com>
2025-03-27 22:18:34 -0400

    method reload

---
+++ Four/src/biz/baijing/methods/listt.java
@@ -0,0 +1,21 @@
+package biz.baijing.methods;
+
+public class listt {
+    public static void main(String[] args) {
+        list1();
+    }
+
+    public static void list1() {
+        list2();
+        System.out.println("1");
+        list3();
+    }
+
+    public static void list2() {
+        System.out.println("2");
+    }
+
+    public static void list3() {
+        System.out.println("3");
+    }
+}
--- Four/src/biz/baijing/methods/reload.java
+++ Four/src/biz/baijing/methods/reload.java
@@ -2,10 +2,19 @@ package biz.baijing.methods;
 
 public class reload {
     public static void main(String[] args) {
-
+        reLoad(12);
+        reLoad2(12 , 10);
     }
 
+    public static int reLoad(int n) {
+        System.out.println(n);
+        return n;
+    }
 
+    public static int reLoad2(int n,int m) {
+        System.out.println(n +" " + m);
+        return  n+m;
+    }
 
 
 }
