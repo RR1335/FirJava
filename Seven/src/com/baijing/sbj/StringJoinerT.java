d1b667cf048f9b5b8d68d36f017fe49673b5bd15
RR1335 <odyli@126.com>
2025-03-30 06:24:40 -0400

    Create StringJoinerT.java

---
+++ Seven/src/com/baijing/sbj/StringJoinerT.java
@@ -0,0 +1,16 @@
+package com.baijing.sbj;
+
+import java.util.StringJoiner;
+
+public class StringJoinerT {
+    public static void main(String[] args) {
+        StringJoiner sj = new StringJoiner(",","{","}");
+
+        String str = sj.add("1").add("2").toString();
+        System.out.println(str);
+
+        int len = sj.length();
+        System.out.println(len);
+
+    }
+}
