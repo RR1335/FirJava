0427d093818beb0cc9c972364a1d7efb04523dda
RR1335 <odyli@126.com>
2025-03-30 06:49:18 -0400

    Update StringJoinerT.java

--- Seven/src/com/baijing/sbj/StringJoinerT.java
+++ Seven/src/com/baijing/sbj/StringJoinerT.java
@@ -4,7 +4,8 @@ import java.util.StringJoiner;
 
 public class StringJoinerT {
     public static void main(String[] args) {
-        StringJoiner sj = new StringJoiner(",","{","}");
+//        StringJoiner sj = new StringJoiner(",","{","}");
+        StringJoiner sj = new StringJoiner("");
 
         String str = sj.add("1").add("2").toString();
         System.out.println(sj.getClass());
