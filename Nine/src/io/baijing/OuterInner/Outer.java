445382e8625fbbe22310c326b7d22e19a037d084
RR1335 <odyli@126.com>
2025-04-02 12:04:52 -0400

    varible , inner class

--- Nine/src/io/baijing/OuterInner/Outer.java
+++ Nine/src/io/baijing/OuterInner/Outer.java
@@ -2,6 +2,13 @@ package io.baijing.OuterInner;
 
 public class Outer {
     private static int a = 10;
+    public Outer() {
+        System.out.println("() -" +System.identityHashCode(a));
+    }
+    public void show() {
+        System.out.println(a);
+        System.out.println("show -" +System.identityHashCode(a));
+    }
 
     static class Inner {
         private int a = 20;
@@ -9,8 +16,11 @@ public class Outer {
         public void show() {
             int a =30;
             System.out.println(a);
+            System.out.println(" -" +System.identityHashCode(a));
             System.out.println(this.a);
+            System.out.println("this -" + System.identityHashCode(this.a));
             System.out.println(Outer.a);
+            System.out.println("Outer -" + System.identityHashCode(Outer.a));
         }
     }
 }
--- Nine/src/io/baijing/OuterInner/RunOuter.java
+++ Nine/src/io/baijing/OuterInner/RunOuter.java
@@ -3,6 +3,9 @@ package io.baijing.OuterInner;
 public class RunOuter {
     public static void main(String[] args) {
 
+        Outer o = new Outer();
+        o.show();
+
         Outer.Inner ooi = new Outer.Inner();
         ooi.show();
     }
