e33b2c3f5cfd735e37394e591b367c2e6160d985
RR1335 <odyli@126.com>
2025-04-02 06:44:29 -0400

    interface update

--- Nine/src/in/baijing/interfaceabstract/InterT.java
+++ Nine/src/in/baijing/interfaceabstract/InterT.java
@@ -4,5 +4,18 @@ public class InterT implements Inter {
     @Override
     public void InterMethod() {
         System.out.println(" InterT implements —— 打印");
+        Inter.end();
     }
+
+    // 接口多态
+//    public int startInter(Inter i) {
+//
+//        i.InterMethod();
+//        i.endT();
+//        Inter.start();
+//        Inter.end();
+//
+//        return 10;
+//
+//    }
 }
--- Nine/src/in/baijing/interfaceabstract/InterTwo.java
+++ Nine/src/in/baijing/interfaceabstract/InterTwo.java
@@ -1,14 +1,12 @@
 package in.baijing.interfaceabstract;
 
+
 public class InterTwo   {
     public static void main(String[] args) {
 
         InterT t = new InterT();
+
         t.InterMethod();
-
-        Inter.start();
-        Inter.end();
-
 
     }
 }
