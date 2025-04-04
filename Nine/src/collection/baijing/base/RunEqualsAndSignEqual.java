511f1f7af90509af60e65bb19d66db3279b16f80
RR1335 <odyli@126.com>
2025-04-04 10:56:20 -0400

    Update RunEqualsAndSignEqual.java

--- Nine/src/collection/baijing/base/RunEqualsAndSignEqual.java
+++ Nine/src/collection/baijing/base/RunEqualsAndSignEqual.java
@@ -1,25 +1,55 @@
 package collection.baijing.base;
 
+import java.util.Objects;
+
 public class RunEqualsAndSignEqual {
     public static void main(String[] args) {
 
-        String x = "string";
-        String y = "string";
-        String z = new String("string");
-        System.out.println(x==y); // true
-        System.out.println(x==z); // false
-        System.out.println(x.equals(y)); // true
-        System.out.println(x.equals(z)); // true
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
 
-        System.out.println("x -" + System.identityHashCode(x));
-        System.out.println("y -" + System.identityHashCode(y));
+//        String x = "string";
+//        String y = "string";
+//        String z = new String("string");
+//        System.out.println(x==y); // true
+//        System.out.println(x==z); // false
+//        System.out.println(x.equals(y)); // true
+//        System.out.println(x.equals(z)); // true
+//
+//        System.out.println("x -" + System.identityHashCode(x));
+//        System.out.println("y -" + System.identityHashCode(y));
+//
+//        System.out.println("z -" + System.identityHashCode(z));
+//
+//        String s = new String("string");
+//        System.out.println("s -" + System.identityHashCode(s));
+//        System.out.println(s.equals(z));
 
-        System.out.println("z -" + System.identityHashCode(z));
+        int[] arr2 = new int[5];
+        arr2[0] = 1;
+        arr2[1] = 2;
+        arr2[2] = 3;
+        arr2[3] = 4;
+        arr2[4] = 5;
 
-        String s = new String("string");
-        System.out.println("s -" + System.identityHashCode(s));
-        System.out.println(s.equals(z));
 
+        System.out.println(arr[0] == arr2[0]);
+        System.out.println(arr[0] == arr2[3]);
+        System.out.println(System.identityHashCode(arr));
+        System.out.println(System.identityHashCode(arr2));
+        System.out.println(System.identityHashCode(arr[0]));
+        System.out.println(System.identityHashCode(arr2[0]));
+
+        System.out.println(System.identityHashCode(arr2[3]));
+
+        System.out.println(arr.equals(arr2));
+        System.out.println(arr2.equals(arr));
+
+        System.out.println(Objects.equals(arr[0], arr2[0]));
 
     }
 }
