c8db76268a889858ac9a8c700295d6231f365df4
RR1335 <odyli@126.com>
2025-04-09 05:47:47 -0400

    stream readme

--- Ten/src/stream/baijing/base/RunArrayStream.java
+++ Ten/src/stream/baijing/base/RunArrayStream.java
@@ -1,6 +1,7 @@
 package stream.baijing.base;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class RunArrayStream {
     public static void main(String[] args) {
@@ -8,6 +9,14 @@ public class RunArrayStream {
         int[] arr = {1,2,3,4,5,6,7,8,9};
 
         Arrays.stream(arr).forEach(System.out::println);
-        
+
+        String[] str = {"a","b","c","d","e","f","g","h","i","j","k","l","m"};
+
+        Arrays.stream(str).forEach(System.out::println);
+
+        Arrays.stream(str,3,8).forEach(System.out::println);
+
+        Stream.of(1,2,3,4,5,6,7,8,9).forEach(System.out::println);
+
     }
 }
