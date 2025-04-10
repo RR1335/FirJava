436b8f4f800b0daadbab40e34ac0e1161ecdead4
RR1335 <odyli@126.com>
2025-04-10 11:03:31 -0400

    Update RunListMethod.java

--- Eleven/src/file/baijing/listmethods/RunListMethod.java
+++ Eleven/src/file/baijing/listmethods/RunListMethod.java
@@ -1,6 +1,7 @@
 package file.baijing.listmethods;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -13,12 +14,26 @@ public class RunListMethod {
 
         File f = new File("/Users/ann/DJava/testfile/t2");
         // 只会获取名字
-        String[] list = f.list();
-        for (String s : list) {
-            System.out.println(s);
-        }
+//        String[] list = f.list();
+//        for (String s : list) {
+//            System.out.println(s);
+//        }
 
-        
+        String[] list1 = f.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+/*                //返回值： [.DS_Store, test, c1.txt, c2.txt, tt1]
+                return true;*/
+
+                File fl = new File(dir, name);
+                // 依次打印 name 的绝对路径
+//                System.out.println(fl);
+//                return true;
+                return fl.getName().endsWith(".txt");
+//                return fl.isFile() && name.endsWith(".txt");
+            }
+        });
+//        System.out.println(Arrays.toString(list1));
 
 
     }
