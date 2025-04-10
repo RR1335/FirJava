aa13dc51fdd59d19df3e97895d2d31202f8d99d2
RR1335 <odyli@126.com>
2025-04-09 22:59:10 -0400

    Update RunTryCatch.java

--- Eleven/src/trycatch/baijing/base/RunTryCatch.java
+++ Eleven/src/trycatch/baijing/base/RunTryCatch.java
@@ -5,13 +5,22 @@ public class RunTryCatch {
 
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        try {
+/*        try {
             System.out.println(arr[12]); // ArrayIndexOutOfBoundsException
             System.out.println(arr[3]/0); // ArithmeticException
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println( "数据越界。" + e);
         } catch (ArithmeticException e) {
             System.out.println("算数异常, 除数不能为 0 ." + e);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }*/
+
+        try {
+            System.out.println(arr[12]); // ArrayIndexOutOfBoundsException
+            System.out.println(arr[3]/0); // ArithmeticException
+        } catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
+            System.out.println( "数据越界。" + e);
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
