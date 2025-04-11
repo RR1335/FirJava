14a88f035db75a47076925cac9dcae183f88c3f7
RR1335 <odyli@126.com>
2025-04-11 05:37:36 -0400

    Update ByteFileInputStreamRun.java

--- Eleven/src/io/baijing/base/ByteFileInputStreamRun.java
+++ Eleven/src/io/baijing/base/ByteFileInputStreamRun.java
@@ -6,11 +6,17 @@ import java.io.IOException;
 public class ByteFileInputStreamRun {
     public static void main(String[] args) throws IOException {
 
-        FileInputStream fis = new FileInputStream("/Users/ann/DJava/testfile/test/a.txt");
+        FileInputStream fis = new FileInputStream("");
+        try {
+            fis = new FileInputStream("/Users/ann/DJava/testfile/test/c.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        // 读取结束，返回 -1
         int bycontent = fis.read();
 
-        System.out.println(bycontent);
+        System.out.println((char) bycontent);
 
         fis.close();
 
