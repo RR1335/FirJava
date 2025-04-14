51b8c3b9f57f976c986da38c6b53649ae31ec524
RR1335 <odyli@126.com>
2025-04-14 10:09:22 -0400

    Update ConvertStreamRun.java

--- Eleven/src/ioimprove/baijing/inputstreamoutputstream/ConvertStreamRun.java
+++ Eleven/src/ioimprove/baijing/inputstreamoutputstream/ConvertStreamRun.java
@@ -1,7 +1,9 @@
 package ioimprove.baijing.inputstreamoutputstream;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class ConvertStreamRun {
@@ -11,7 +13,9 @@ public class ConvertStreamRun {
         // 指定字符编码读取数据
         FileReader fr = new FileReader(FRFLODER, StandardCharsets.UTF_8); // Charset.forName("UTF_8")
 
+        FileWriter fw = new FileWriter(FRFLODER, Charset.forName("GBK"),true);
 
+//        FileWriter fw = new FileWriter(FRFLODER, StandardCharsets.UTF_8,true); // StandardCharsets 仅限 UTF_
 
     }
 }
