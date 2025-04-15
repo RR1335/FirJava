681236a722813f10a5eb923f3a17572b0b5377a9
RR1335 <odyli@126.com>
2025-04-15 07:21:34 -0400

    Update SystemOutRun.java

--- Eleven/src/printstream/baijing/system/SystemOutRun.java
+++ Eleven/src/printstream/baijing/system/SystemOutRun.java
@@ -7,14 +7,14 @@ public class SystemOutRun {
 
         // 标准输出流 public static final PrintStream out = null;
         // 本身就是 PrintStream
-        // 无法关闭，如果关闭则需要重启 jvm 
+        // 无法关闭，如果关闭则需要重启 jvm
         PrintStream ps = System.out;
 
         // jvm 默认打印到控制台 Console
         // System.out.println
         ps.println("这是标准输出流！");
 
-
+        ps.close(); // 错误的，
 
     }
 }
