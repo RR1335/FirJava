128d61d2cf52e51418e972bdb2140f9d914e098a
RR1335 <odyli@126.com>
2025-04-19 15:13:48 -0400

    Create IOUtilsRun.java

---
+++ Eleven/src/commonsio/baijing/ioutils/IOUtilsRun.java
@@ -0,0 +1,16 @@
+package commonsio.baijing.ioutils;
+
+import java.io.IOException;
+
+public class IOUtilsRun {
+    public static void main(String[] args) throws IOException {
+
+/*        public static int copy(InputStream input, OutputStream output); // 复制文件
+        public static int copyLarge(Reader input,Writer output); // 复制大文件
+        public static String readLines(Reader input); // 读取数据
+        public static voide writer(String data,OutputStream output); // 写出数据*/
+
+
+
+    }
+}
