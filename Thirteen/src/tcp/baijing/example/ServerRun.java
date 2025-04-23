f227c0b5d637a4b43a049d020e425fc2cb1d75bd
RR1335 <odyli@126.com>
2025-04-22 22:33:51 -0400

    TCP , Client and Server

---
+++ Thirteen/src/tcp/baijing/example/ServerRun.java
@@ -0,0 +1,31 @@
+package tcp.baijing.example;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerRun {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket ss = new ServerSocket(20001);
+
+        Socket socket = ss.accept();
+        // 将字节流转成字符流
+        InputStreamReader ir = new InputStreamReader(socket.getInputStream());
+        int ch;
+        while ((ch = ir.read() ) != -1) {
+            System.out.print((char) ch);
+        }
+
+        // 服务器回写数据
+        String str = "Receive ...";
+        OutputStream os = socket.getOutputStream();
+        os.write(str.getBytes());
+
+        socket.close();
+        ss.close();
+
+    }
+}
